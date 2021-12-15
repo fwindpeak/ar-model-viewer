851a145aa9103639a9dd9253d345ec2111cdbc01
fwindpeak <fwindpeak@gmail.com>
2021-12-15 11:36:08 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import model from './assets/NeilArmstrong.glb?url';
+import poster from './assets/NeilArmstrong.webp';
+import environment from './assets/moon_1k.hdr?url';
+</script>
+
+<template>
+  <model-viewer
+    alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
+    :src="model"
+    ar
+    ar-modes="webxr scene-viewer quick-look"
+    :environment-image="environment"
+    :poster="poster"
+    seamless-poster
+    shadow-intensity="1"
+    camera-controls
+    class="model-viewer"
+  ></model-viewer>
+</template>
+
+<style>
+#app .model-viewer {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import '@google/model-viewer';
+createApp(App).mount('#app');
